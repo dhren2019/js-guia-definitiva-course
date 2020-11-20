5b02ed515791dc91efe855dc1873de08f2db7dfe
[Rodrigo] <[roglobar10@gmail.com]>
2020-11-20 19:11:58 +0100

    Commits

--- 39-CRIPTOMONEDAS/js/app.js
+++ 39-CRIPTOMONEDAS/js/app.js
@@ -1,5 +1,6 @@
 // const selectCriptomonedas = document.querySelector('#criptomonedas');
 const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
 const formulario   = document.querySelector('#formulario');
 
 const objBusqueda = {
@@ -19,6 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
     formulario.addEventListener('submit',  submitFormulario);
 
     criptomonedasSelect.addEventListener('change', leerValor);
+    monedaSelect.addEventListener('change', leerValor);
 })
 function consultarCriptomoneda() {
     
@@ -45,9 +47,52 @@ function selectCriptomonedas(criptomonedas) {
 function leerValor(e){
 
     objBusqueda[e.target.name] = e.target.value;
-    console.log(objBusqueda);
+    // console.log(objBusqueda);
 }
 
 function submitFormulario(e) {
     e.preventDefault();
+
+    // validar
+    const { moneda, criptomoneda} = objBusqueda;
+
+    if (moneda === '' || criptomoneda === '') {
+        mostrarAlerta('Ambos campos son obligatorios');
+        return;
+    }
+
+    // Consultar la API con los resultados
+
+    consultarApi();
+}
+
+function mostrarAlerta(msg) {
+
+    const existeError = document.querySelector('.error');
+
+    if (!existeError) {
+        
+        const divMensaje  = document.createElement('div');
+        divMensaje.classList.add('error');
+    
+        // mensaje de error
+        divMensaje.textContent = msg;
+        formulario.appendChild(divMensaje);
+    
+        setTimeout(() => {
+            divMensaje.remove();
+        },3000);
+    }
+}
+
+function consultarApi(){
+    const {moneda, criptomoneda} = objBusqueda;
+
+    const url = `https://min-api.cryptocompare.com/data/price?fsym=${criptomoneda}&tsyms=${moneda}`;
+
+    fetch(url)
+    .then( respuesta => respuesta.json())
+    .then( cotizacion => {
+        console.log(cotizacion)
+    })
 }
